44c2943835ecab23c9ef74b1264f85e5d1812c1b
oliverzli <oliverzli@tencent.com>
2021-04-07 22:06:58 +0800

    Reduce img

--- cos.js
+++ cos.js
@@ -43,7 +43,7 @@ const uploadFiles = async (items) => {
         items = traverseDirSync(SOURCE_PATH, {
             nodir: true,
             filter: ({ path, stats }) => {
-                if (!stats.isDirectory() && path.indexOf('/img/background-') > 0) {
+                if (stats.isDirectory() || (!stats.isDirectory() && path.indexOf('/img/background-') > 0)) {
                     return false
                 }
                 return true;
